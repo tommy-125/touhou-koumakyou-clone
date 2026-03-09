8a7229865a79358fc2c8283685879778f6185d2f
tommy-125 <hhhvftghf@gmail.com>
2026-03-09 21:18:00 +0800

    chore: let cmake export compile commands for clangd analyze tool / customized window title and size

---
+++ config/config.hpp
@@ -0,0 +1,25 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#include "pch.hpp" // IWYU pragma: export
+
+#include "Util/Logger.hpp"
+
+constexpr const char *TITLE = "Touhou Koumakyou";
+
+constexpr int WINDOW_POS_X = SDL_WINDOWPOS_UNDEFINED;
+constexpr int WINDOW_POS_Y = SDL_WINDOWPOS_UNDEFINED;
+
+constexpr unsigned int WINDOW_WIDTH = 640;
+constexpr unsigned int WINDOW_HEIGHT = 480;
+
+constexpr Util::Logger::Level DEFAULT_LOG_LEVEL = Util::Logger::Level::DEBUG;
+
+/**
+ * @brief FPS limit
+ *
+ * Set value to 0 to turn off FPS cap
+ */
+constexpr unsigned int FPS_CAP = 60;
+
+#endif
